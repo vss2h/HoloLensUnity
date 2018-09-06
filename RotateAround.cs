7129e2296a64a639abce7acc7239f4932cfb8c5f
myru31 <36711263+myru31@users.noreply.github.com>
2018-09-06 08:27:03 -0500

    Update RotateAround.cs

--- RotateAround.cs
+++ RotateAround.cs
@@ -12,6 +12,8 @@ public class RotateAround : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        transform.RotateAround(Vector3.zero,Vector3.up,3*Time.deltaTime);
+        transform.RotateAround(Vector3.zero,Vector3.up,3*Time.deltaTime); /* Game Object revolves around a point in space.
+	Needs arguments (point, axis of revolution, degrees/time).  
+	Vector3.zero is shorthand for (0,0,0) and Vector3.up is (0,1,0) */
 	}
 }
