c1031424ccb59cb1ebbee3e1a92980762e62b443
vss2h <vakise@hotmail.fr>
2018-09-20 00:38:33 -0500

    Add files via upload

---
+++ Assets/Scripts/Rotation/RotateAround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.down, 45*Time.deltaTime); /* Game Object revolves around a point in space.
+	Needs arguments (point, axis of revolution, degrees/time).  
+	Vector3.zero is shorthand for (0,0,0) and Vector3.up is (0,1,0) */
+	}
+}
---
+++ Assets/Scripts/camera/SwitchCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour {
+
+    //public Input BatteryStatus = Input.GetKeyUp(KeyCode.UpArrow);
+
+    public Camera overheadCam;
+    public Camera closeCam;
+    public Camera closeCam2;
+
+    // Use this for initialization
+
+     void Start ()
+    {
+        overheadCam.enabled = true;
+        closeCam.enabled = false;
+        closeCam2.enabled = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            overheadCam.enabled = false;
+            closeCam.enabled = true;
+            closeCam2.enabled = false;
+        }
+            
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            overheadCam.enabled = false;
+            closeCam.enabled = false;
+            closeCam2.enabled = true;
+        }
+            //ShowCloseCam1();
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            overheadCam.enabled = true;
+            closeCam.enabled = false;
+            closeCam2.enabled = false;
+        }
+         
+    }
+}
